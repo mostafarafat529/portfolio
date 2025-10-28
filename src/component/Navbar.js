9549106eb2acaf29f278706276ddf0db7c787657
mostafa rafat <mostafarafat@gmail.com>
2025-10-28 16:33:42 +0200

    first change

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,15 +6,16 @@ const Navbar = () => {
         <div className='first-section'>
 <Container>
         <div className='header'>
-            <h1>portfolio</h1>
-            <nav>
-    <a href="#home" className='active'>Home</a>
-    <a href="#about">About</a>
-    <a href="#services">Services</a>
-    <a href="#skills">Skills</a>
-    <a href="#portfolio">Portfolio</a>
-    <a href="#contact">Contact</a>
+        <h1>portfolio</h1>
+        <nav>
+<a href="#home" className='active'>Home</a>
+<a href="#about">About</a>
+<a href="#services">Services</a>
+<a href="#skills">Skills</a>
+<a href="#portfolio">Portfolio</a>
+<a href="#contact">Contact</a>
 </nav>
+<button> <a href="/PDF/ATS CV ME.pdf"target="_blank" download="CV MOSTAFA RAFAT">Download CV</a></button>
 
         </div>
 </Container>
